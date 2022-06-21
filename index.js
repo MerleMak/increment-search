dd524a65b31e910ee6f7268661223adce4c32eb8
Merle Makoben <merle.makoben@neuefische.de>
2022-06-21 16:26:01 +0200

    Edit formatting

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-import countries from "./countries.json" assert {type: "json"};
+import countries from './countries.json' assert { type: 'json' };
 
-const countrylist = document.querySelector("[data-js=countrylist]")
-const searchfield = document.querySelector("[data-js=searchfield]")
-const body = document.body
+const countrylist = document.querySelector('[data-js=countrylist]');
+const searchfield = document.querySelector('[data-js=searchfield]');
+const body = document.body;
 
 searchfield.addEventListener('input', (event) => {
-    const searchQuery = event.target.value 
-    countrylist.classList.remove("hidden")
-    countrylist.innerHTML = ""
-    const matchingCountries = countries.filter(country => {
-        return country.toLowerCase().startsWith(searchQuery.toLowerCase())
-    }) 
-    matchingCountries.slice(0,4).forEach(country => {
-        const newCountry = countryComponent(country)
-        countrylist.appendChild(newCountry)
-    })
-    if (matchingCountries.length === 0){
-        const noResult = document.createElement("li")
-        noResult.textContent = "no results"
-        noResult.classList.add("no-result")
-        countrylist.appendChild(noResult)
-    }
-})
+  const searchQuery = event.target.value;
+  countrylist.classList.remove('hidden');
+  countrylist.innerHTML = '';
+  const matchingCountries = countries.filter((country) => {
+    return country.toLowerCase().startsWith(searchQuery.toLowerCase());
+  });
+  matchingCountries.slice(0, 4).forEach((country) => {
+    const newCountry = countryComponent(country);
+    countrylist.appendChild(newCountry);
+  });
+  if (matchingCountries.length === 0) {
+    const noResult = document.createElement('li');
+    noResult.textContent = 'no results';
+    noResult.classList.add('no-result');
+    countrylist.appendChild(noResult);
+  }
+});
+
 searchfield.addEventListener('click', (event) => {
-    event.stopPropagation()
-    countrylist.classList.remove("hidden")
-})
+  event.stopPropagation();
+  countrylist.classList.remove('hidden');
+});
+
+searchfield.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowDown':
+      countrylist.firstChild?.firstChild?.focus();
+      event.preventDefault();
+      break;
+    default:
+      break;
+  }
+});
 
 body.addEventListener('click', () => {
-    countrylist.classList.add("hidden")
-})
+  countrylist.classList.add('hidden');
+});
 
-function countryComponent (country){
-    const newCountry = document.createElement("li")
-        const countryButton = document.createElement("button")
-        countryButton.textContent = country
-        countryButton.addEventListener('click', () => {
-            searchfield.value = country
-        })
-        newCountry.appendChild(countryButton)
-        return newCountry
+function countryComponent(country) {
+  const newCountry = document.createElement('li');
+  const countryButton = document.createElement('button');
+  countryButton.textContent = country;
+  countryButton.addEventListener('click', () => {
+    searchfield.value = country;
+  });
+  countryButton.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        countryButton.parentElement.previousSibling?.firstChild.focus();
+        event.preventDefault();
+        break;
+      case 'ArrowDown':
+        countryButton.parentElement.nextSibling?.firstChild.focus();
+        event.preventDefault();
+        break;
+      default:
+        break;
+    }
+  });
+  newCountry.appendChild(countryButton);
+  return newCountry;
 }
