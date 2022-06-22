4583954c01dfcaebc3abe82e4fbceeacbd0d47b5
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-06-22 10:59:33 +0200

    Add easy fetch solution

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-import countries from './countries.json' assert { type: 'json' };
-
-const countrylist = document.querySelector('[data-js=countrylist]');
-const searchfield = document.querySelector('[data-js=searchfield]');
+const countrylist = document.querySelector("[data-js=countrylist]");
+const searchfield = document.querySelector("[data-js=searchfield]");
 const body = document.body;
 
-searchfield.addEventListener('input', (event) => {
+searchfield.addEventListener("input", (event) => {
   const searchQuery = event.target.value;
-  countrylist.classList.remove('hidden');
-  countrylist.innerHTML = '';
-  const matchingCountries = countries.filter((country) => {
-    return country.toLowerCase().startsWith(searchQuery.toLowerCase());
+  countrylist.classList.remove("hidden");
+  countrylist.innerHTML = "";
+  fetchCountries(searchQuery).then((data) => {
+    const matchingCountries = data;
+
+    matchingCountries.slice(0, 4).forEach((country) => {
+      const newCountry = countryComponent({ country });
+      countrylist.appendChild(newCountry);
+    });
+
+    if (matchingCountries.length === 0) {
+      const noResult = document.createElement("li");
+      noResult.textContent = "no results";
+      noResult.classList.add("no-result");
+      countrylist.appendChild(noResult);
+    }
   });
-  matchingCountries.slice(0, 4).forEach((country) => {
-    const newCountry = countryComponent(country);
-    countrylist.appendChild(newCountry);
-  });
-  if (matchingCountries.length === 0) {
-    const noResult = document.createElement('li');
-    noResult.textContent = 'no results';
-    noResult.classList.add('no-result');
-    countrylist.appendChild(noResult);
-  }
 });
 
-searchfield.addEventListener('click', (event) => {
+searchfield.addEventListener("click", (event) => {
   event.stopPropagation();
-  countrylist.classList.remove('hidden');
+  countrylist.classList.remove("hidden");
 });
 
-searchfield.addEventListener('keydown', (event) => {
+searchfield.addEventListener("keydown", (event) => {
   switch (event.key) {
-    case 'ArrowDown':
+    case "ArrowDown":
       countrylist.firstChild?.firstChild?.focus();
       event.preventDefault();
       break;
@@ -39,31 +39,48 @@ searchfield.addEventListener('keydown', (event) => {
   }
 });
 
-body.addEventListener('click', () => {
-  countrylist.classList.add('hidden');
+body.addEventListener("click", () => {
+  countrylist.classList.add("hidden");
 });
 
-function countryComponent(country) {
-  const newCountry = document.createElement('li');
-  const countryButton = document.createElement('button');
-  countryButton.textContent = country;
-  countryButton.addEventListener('click', () => {
-    searchfield.value = country;
-  });
-  countryButton.addEventListener('keydown', (event) => {
+function countryComponent({ country }) {
+  const newCountry = document.createElement("li");
+
+  function handleKeyDown(event) {
     switch (event.key) {
-      case 'ArrowUp':
+      case "ArrowUp":
         countryButton.parentElement.previousSibling?.firstChild.focus();
         event.preventDefault();
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
         countryButton.parentElement.nextSibling?.firstChild.focus();
         event.preventDefault();
         break;
       default:
         break;
     }
+  }
+
+  const countryButton = buttonComponent({
+    yourButtonTextHere: country,
+    onClick: () => (searchfield.value = country),
+    onKeyDown: handleKeyDown,
   });
+
   newCountry.appendChild(countryButton);
   return newCountry;
+}
+
+function buttonComponent({ yourButtonTextHere, onClick, onKeyDown }) {
+  const countryButton = document.createElement("button");
+  countryButton.textContent = yourButtonTextHere;
+  countryButton.addEventListener("click", onClick);
+  countryButton.addEventListener("keydown", onKeyDown);
+  return countryButton;
+}
+
+function fetchCountries(query) {
+  const result = fetch(`https://spicedworld.herokuapp.com/?q=${query}`);
+  const data = result.then((res) => res.json());
+  return data;
 }
